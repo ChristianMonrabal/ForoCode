3623d1146f06ba0eb680a09fd2535f06f8725fdf
Christian Monrabal <90915794+ChristianMonrabal@users.noreply.github.com>
2024-12-03 12:14:04 +0100

    Update edit_question.php

--- public/edit_question.php
+++ public/edit_question.php
@@ -133,5 +133,6 @@ try {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../js/form.js"></script>
+    <script src="../js/validation_question.js"></script>
 </body>
 </html>
