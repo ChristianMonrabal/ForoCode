1ecbc7531ae294b77d6bf87cbeb66c90b6dfbffb
Christian Monrabal <90915794+ChristianMonrabal@users.noreply.github.com>
2024-12-03 12:14:44 +0100

    Update view_question.php

--- public/view_question.php
+++ public/view_question.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 require '../db/conexion.php';
@@ -143,5 +144,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../js/form.js"></script>
+    <script src="../js/validation_question.js"></script>
 </body>
 </html>
